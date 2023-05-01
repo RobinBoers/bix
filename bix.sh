f6765281e29666207780c728b59a3a49c784de12
Robin Boers <robindev2019@outlook.com>
2023-05-01 14:28:07 +0200

    Fix presedence in error handling

--- bix.sh
+++ bix.sh
@@ -191,9 +191,19 @@ function add-remote -a remote_repo --description "add-remote <repo>"
   set remote origin
 
   git remote show $remote > /dev/null 2> /dev/null && delete-remote $remote 
-  git remote add $remote "$BIX_GIT_HOST_SSH:/$remote_repo" || delete-remote $remote && error "Uh oh, that didn't work! Are you sure the remote exists? (It should be 'user/repo', not just 'repo')"
+  git remote add $remote "$BIX_GIT_HOST_SSH:/$remote_repo"
+  
+  if test $status != 0
+    delete-remote $remote
+    error "Uh oh, that didn't work! Are you sure the remote exists? (It should be 'user/repo', not just 'repo')"
+  end   
     
-  git push -u $remote $BIX_GIT_DEFAULT_BRANCH || delete-remote $remote && error "Uh oh, couldn't push! Are you sure the remote exists and the path is correct? (It should be 'user/repo', not just 'repo')"
+  git push -u $remote $BIX_GIT_DEFAULT_BRANCH
+
+  if test $status != 0
+    delete-remote $remote
+    error "Uh oh, couldn't push! Are you sure the remote exists and the path is correct? (It should be 'user/repo', not just 'repo')"
+  end
   
   success "🦑 Set up new remote $remote for you :)"
 end
