c14133d8e31b4889c3ee3ee1ad1b1b7d10eb26c8
Robin Boers <robindev2019@outlook.com>
2023-08-05 17:49:54 +0200

    Add notice about PATH in install.sh

--- install.sh
+++ install.sh
@@ -9,4 +9,6 @@ mkdir (dirname $destination_path)
 wget --quiet https://git.geheimesite.nl/libre0b11/bix/raw/branch/master/bix.sh -O $destination_path
 chmod +x $destination_path
 
+echo "Installed in $destination_dir. Make sure it is available in your PATH"
+
 echo "✨ Ty for trying bix!"
